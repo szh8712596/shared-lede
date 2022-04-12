cbb4b3a36a3732fb6c918d9b1b023a71af5775a0
szh8712596 <62515444+szh8712596@users.noreply.github.com>
2022-04-12 15:50:39 +0800

    Update 3clone.sh

--- preset-models/3clone.sh
+++ preset-models/3clone.sh
@@ -7,9 +7,7 @@ mv ./openwrt/* ./
 cd package
 git clone https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/Ausaci/luci-app-nat6-helper.git
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
-git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
-git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
+git clone https://github.com/kiddin9/openwrt-bypass.git
+git clone https://github.com/pymumu/smartdns.git
